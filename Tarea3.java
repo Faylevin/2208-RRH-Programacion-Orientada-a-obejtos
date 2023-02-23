d09345a2409dd6d74916594ed52c64e0f8721ae7
Ricardo Rojas Herrera <125779806+Faylevin@users.noreply.github.com>
2023-02-22 21:07:31 -0600

    Add files via upload

---
+++ Tarea3.java
@@ -0,0 +1,18 @@
+public class Tarea3 { //Hecho por Rojas Herrera Ricardo 2208
+    public static void main(String[] args) {
+
+        System.out.print("\033[32m"); //Verde
+        System.out.println("- - - - -");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("\033[0m"); //Blanco
+            System.out.println("|       | ");
+        }
+        System.out.print("\033[32m");//Verde
+        System.out.println("- - - - -");
+
+
+            }
+        }
+
+
+
